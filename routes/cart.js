a21e2c0375239b36533f6288b9437d482d0dd1db
Oleksandr Shtuka <ashtuka@gmail.com>
2020-01-29 23:35:25 +0200

    create view for cart

--- routes/cart.js
+++ routes/cart.js
@@ -2,20 +2,28 @@ const {Router} = require('express');
 const Course = require('../models/course');
 const router = Router();
 
+const mapCartItems = cart => cart.items.map(item => ({...item.courseId._doc, count: item.count}));
+
+const computePrice = courses => courses.reduce((total, course) => total += course.price * course.count, 0);
+
 router.post('/add', async (req, res) => {
     const course = await Course.findById(req.body.id);
     await req.user.addToCart(course);
     res.redirect('/cart');
 });
 
-router.get('/', (req, res) => {
-   // const cart = await Cart.fetch();
-   // res.render('cart',
-   //     {title: 'Cart',
-   //      isCart: true,
-   //      courses: cart.courses,
-   //      price: cart.price})
-    res.json({test: true})
+router.get('/', async (req, res) => {
+   const user = await req.user
+       .populate('cart.items.courseId')
+       .execPopulate();
+
+   const courses = mapCartItems(user.cart);
+
+   res.render('cart',
+       {title: 'Cart',
+        isCart: true,
+        courses: courses,
+        price: computePrice(courses)})
 });
 
 router.delete('/remove/:id', async (req, res) => {
